f27035e2d31a4c879501b87aa16157f7f919fedd
sriram <138786432+sriramfinbot@users.noreply.github.com>
2023-07-18 16:10:27 +0530

    updates
    updates

--- CRUD-MONGO-11-07-2023/routes/home.js
+++ CRUD-MONGO-11-07-2023/routes/home.js
@@ -27,15 +27,14 @@ Router.post("/add", (req, res) => {
             return res.status(400).json({ error: err })
         }
 
-        let oldPath=file.photo[0].filepath;
-        let newPath = path.join("E:", "CRUD-MONGO-11-07-2023", "public", 'uploads') + '/' + Date.now() + "-"+ file.photo[0].originalFilename;
-        
-        photo = Date.now() + "-" + file.photo[0].originalFilename;
-        console.log(photo);
+        let oldPath = file.photo[0].filepath;
+        let newPath = path.join("E:", "CRUD-MONGO-11-07-2023", "public", 'uploads') + '/' + Date.now() + "-" + file.photo[0].originalFilename;
 
-        let rawData=fs.readFileSync(oldPath)
-        fs.writeFile(newPath,rawData,function(err){
-            if(err){
+        photo = Date.now() + "-" + file.photo[0].originalFilename;
+
+        let rawData = fs.readFileSync(oldPath)
+        fs.writeFile(newPath, rawData, function (err) {
+            if (err) {
                 console.log(err);
             }
         })
@@ -55,15 +54,15 @@ Router.post("/add", (req, res) => {
         user.photo = photo;
 
 
-            user.save().then(() => {
-                console.log('Data saved to mongodb successfully');
-                res.redirect("/")
-            }).catch((err) => {
-                console.log(err);
-                console.log("error happened")
-            })
-
+        user.save().then(() => {
+            console.log('Data saved to mongodb successfully');
+            res.redirect("/")
+        }).catch((err) => {
+            console.log(err);
+            console.log("error happened")
         })
+
+    })
 })
 
 // find data 
@@ -80,27 +79,90 @@ Router.get("/show", (req, res) => {
 
 // update data showing
 Router.get("/edit/:id", (req, res) => {
+    // console.log(req.params.id);
+    // console.log(req.params.id);
+    // console.log(req.originalUrl);
     User.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true }).then((docs) => {
         res.render("edit", { studentData: docs })
     }).catch((err) => {
+        // console.log(err);
         console.log("can't update")
     })
 })
 
 // for updating
 Router.post("/edit/:id", (req, res) => {
-    User.findByIdAndUpdate({ _id: req.params.id }, req.body).then((docs) => {
-        res.redirect("/show")
-    }).catch((err) => {
-        console.log("error");
-        console.log(err);
+
+    console.log(req.body);
+
+    const form = new formidable.IncomingForm();
+    form.options.allowEmptyFiles = true;
+
+    form.parse(req, (err, fields, file) => {
+
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ error: err })
+        }
+
+        let photo = "";
+
+        if (Object.keys(file).length == 0) {
+            User.findById({ _id: req.params.id }, req.body).then((docs) => {
+                // console.log(docs.photo)
+                photo = docs.photo
+            }).catch((err) => {
+                console.log("error in findById delete", err);
+            })
+        } else {
+            let oldPath = file.photo[0].filepath;
+            let newPath = path.join("E:", "CRUD-MONGO-11-07-2023", "public", 'uploads') + '/' + Date.now() + "-" + file.photo[0].originalFilename;
+
+            photo = Date.now() + "-" + file.photo[0].originalFilename;
+            // console.log(photo);
+
+            let rawData = fs.readFileSync(oldPath)
+            fs.writeFile(newPath, rawData, function (err) {
+                if (err) {
+                    console.log(err);
+                }
+            })
+        }
+
+
+
+        firstName = fields.firstName;
+        firstName = firstName.toString();
+        lastName = fields.lastName;
+        lastName = lastName.toString();
+        email = fields.email;
+        email = email.toString();
+        mob_no = fields.mob_no;
+        mob_no = mob_no.toString();
+
+        var user = {};
+        user.firstName = firstName;
+        user.lastName = lastName;
+        user.email = email;
+        user.mob_no = mob_no;
+        user.photo = photo;
+
+        User.findByIdAndUpdate({ _id: req.params.id }, user).then((docs) => {
+            res.redirect("/show")
+        }).catch((err) => {
+            console.log("error");
+            // console.log(err);
+        })
+
     })
+
+
 })
 
 // delete data
 Router.get("/delete/:id", (req, res) => {
 
-    User.findById({_id: req.params.id}, req.body).then((docs) => {
+    User.findById({ _id: req.params.id }, req.body).then((docs) => {
         console.log(docs.photo);
         let pathForDelete = path.join("E:", "CRUD-MONGO-11-07-2023", "public", 'uploads') + '/' + docs.photo;
         fs.unlinkSync(pathForDelete);
@@ -108,11 +170,11 @@ Router.get("/delete/:id", (req, res) => {
     }).catch((err) => {
         console.log("error in findById delete", err);
     })
-    
+
 
     User.findByIdAndDelete({ _id: req.params.id }, req.body).then((docs) => {
         res.redirect("/show")
-    }).catch((err) => { 
+    }).catch((err) => {
         console.log("error")
     })
 
